53dcd8192937f24cc3e21363183b8aa8554dffcf
wqzhang0 <wqzhang0@163.com>
2016-12-02 08:36:49 +0800

    用fragment 分割上下页面，方便切换页面

--- app/src/main/java/com/wqzhang/thingswapper/MainActivity.java
+++ app/src/main/java/com/wqzhang/thingswapper/MainActivity.java
@@ -1,6 +1,9 @@
 package com.wqzhang.thingswapper;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,45 +15,22 @@ import com.wqzhang.thingswapper.UI.DecorationTest;
 import com.wqzhang.thingswapper.UI.RecyclerAdapter;
 
 
-public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
+public class MainActivity extends Activity   {
 
-    private static RecyclerView recyclerView;
     private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.to_do_list_view);
-        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(this);
-//        线性布局
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
-        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        setContentView(R.layout.to_do_main);
 
-//        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment f = new ToDoFragment();
+        fragmentTransaction.replace(R.id.main_content,f);
+        fragmentTransaction.commit();
 
-//        表格布局 可以自定义 表格行数 水平/垂直 布局  reverseLayout 决定从左到右还是从右至左
-//        GridLayoutManager  gridLayoutManager = new GridLayoutManager(this,3);
-//        gridLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-//        gridLayoutManager.setReverseLayout(true);
-
-//        瀑布布局
-//        StaggeredGridLayoutManager staggeredGridLayoutManager
-//                = new StaggeredGridLayoutManager(4, RecyclerView.VERTICAL);
-
-
-        DecorationTest decorationTest = new DecorationTest(this);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-//        recyclerView.addItemDecoration(decorationTest);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setAdapter(recyclerAdapter);
     }
-
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-        Log.d(TAG, "onItemClick");
-    }
-
 
 
 }
---
+++ app/src/main/java/com/wqzhang/thingswapper/ToDoFragment.java
@@ -0,0 +1,58 @@
+package com.wqzhang.thingswapper;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wqzhang.thingswapper.UI.DecorationTest;
+import com.wqzhang.thingswapper.UI.RecyclerAdapter;
+
+/**
+ * Created by wqzhang on 16-12-1.
+ */
+
+public class ToDoFragment extends Fragment {
+    private String TAG = "ToDoFragment";
+    private static RecyclerView recyclerView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.to_do_fragment, container, false);
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(getActivity());
+//        线性布局
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+//        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+//        表格布局 可以自定义 表格行数 水平/垂直 布局  reverseLayout 决定从左到右还是从右至左
+//        GridLayoutManager  gridLayoutManager = new GridLayoutManager(this,3);
+//        gridLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        gridLayoutManager.setReverseLayout(true);
+
+//        瀑布布局
+//        StaggeredGridLayoutManager staggeredGridLayoutManager
+//                = new StaggeredGridLayoutManager(4, RecyclerView.VERTICAL);
+
+
+        DecorationTest decorationTest = new DecorationTest(getActivity());
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+//        recyclerView.addItemDecoration(decorationTest);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(recyclerAdapter);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+        return view;
+    }
+
+}
